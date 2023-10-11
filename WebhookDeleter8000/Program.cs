9a798107d13714250c42a798e229d8aa6aba47af
Alex_265 <63714415+Alex-265@users.noreply.github.com>
2023-10-11 20:32:38 +0200

    Add files via upload

---
+++ WebhookDeleter8000/Program.cs
@@ -0,0 +1,24 @@
+﻿public class WebhookDeleter {
+    public static void DeleteWebhook(string webhookUrl) {
+        using (var client = new HttpClient()) {
+            var request = new HttpRequestMessage(HttpMethod.Delete, webhookUrl);
+
+            var response = client.SendAsync(request).Result;
+
+            if (response.IsSuccessStatusCode) {
+                Console.WriteLine("Webhook successfully deleted.");
+            } else {
+                Console.WriteLine("Failed to delete webhook.");
+            }
+        }
+    }
+
+    public static void Main(string[] args) {
+        Console.Write("Enter the webhook URL to delete: ");
+        string webhookUrl = Console.ReadLine();
+        Console.Write("\n");
+        DeleteWebhook(webhookUrl);
+        Console.WriteLine("\nPress any Key to exit...");
+        Console.ReadKey();
+    }
+}
